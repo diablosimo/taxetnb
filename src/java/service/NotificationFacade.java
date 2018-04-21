944c16f8b8cdb8706317a38644ad5bc4c31ec6e0
simob <simob@Presario>
2018-04-21 20:56:24 +0100

    21/04/2018  avancement hassna del lib

--- src/java/service/NotificationFacade.java
+++ src/java/service/NotificationFacade.java
@@ -6,6 +6,15 @@
 package service;
 
 import bean.Notification;
+import bean.Redevable;
+import bean.TaxeAnnuelle;
+import bean.Terrain;
+import controller.util.DateUtil;
+import controller.util.SearchUtil;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -19,6 +28,8 @@ public class NotificationFacade extends AbstractFacade<Notification> {
 
     @PersistenceContext(unitName = "taxeTNBPU")
     private EntityManager em;
+@EJB
+TaxeAnnuelleFacade annuelleFacade;
 
     @Override
     protected EntityManager getEntityManager() {
@@ -28,5 +39,79 @@ public class NotificationFacade extends AbstractFacade<Notification> {
     public NotificationFacade() {
         super(Notification.class);
     }
+   
     
+
+    public List<Redevable> findNonPayants(List<Terrain> terrains) {
+        List<Redevable> redevables = new ArrayList();
+        for (int i = 0; i < terrains.size(); i++) {
+            Terrain terrain = terrains.get(i);
+            redevables.add(terrain.getRedevable());
+        }
+        return redevables;
+    }
+    public List<Terrain> notifier(int annee, int typeDernierNot){
+        Integer dureeNot;
+        switch (typeDernierNot) {
+            case 0:
+                dureeNot=null;
+                break;
+            case 1:
+            case 2:
+                dureeNot=30;
+                break;
+            default:
+                return null;
+        }
+        return notifier(annee, typeDernierNot, dureeNot);
+    }
+    
+    public List<Terrain> notifier(int annee, int typeDernierNot, Integer dureeNot) {
+        List<Terrain> res = new ArrayList();
+        Date dateP = DateUtil.parse(annee + "-03-30");
+        Long ldateP = dateP.getTime();
+        Date dateToDay = new Date();
+        Date sdateToDay = DateUtil.convertFormUtilToSql(dateToDay);
+        Long ldateToDay = dateToDay.getTime();
+        if (ldateToDay >= ldateP) {
+            String req = "SELECT te FROM Terrain te WHERE 1=1 ";
+            req += SearchUtil.addConstraint("te", "dernierPaiement.annee", "<", annee);
+            if (dureeNot != null) {
+                req += " AND FUNCTION('DATEDIFF','" + sdateToDay + "',te.DatedernierNotification) > '" + dureeNot + "'";
+            } else {
+                req += SearchUtil.addConstraint("te", "DatedernierNotification", "=", null);
+            }
+            req += SearchUtil.addConstraint("te", "typeDernierNotification", "=", typeDernierNot);
+            System.out.println(req);
+            res = em.createQuery(req).getResultList();
+            System.out.println(res);
+            return res;
+        } else {
+            return null;
+        }
+    }
+    public List<Notification> creerNotification(int annee, int typeDernierNot){
+        List<Terrain> terrains = notifier( annee, typeDernierNot);
+        if(terrains.isEmpty() || terrains ==null){
+            return null;
+        }else{
+            List<Notification>  notifications= new ArrayList();
+            for (int i = 0; i < terrains.size(); i++) {
+                Terrain terrain = terrains.get(i);
+                Notification notification= new Notification();
+                notification.setId(generate("Notification", "id"));
+                notification.setType(typeDernierNot+1);
+                notification.setAnnee(annee);
+                notification.setDateEnvoi(new Date());
+                notification.setDateReception(new Date());
+                notification.setTerrain(terrain);
+                TaxeAnnuelle taxeAnnuelle = new TaxeAnnuelle();
+                taxeAnnuelle =annuelleFacade.createForNotification(terrain, annee);
+                notification.setMontantEstime(taxeAnnuelle.getMontantTotal());
+                notification.setNombreMoisRetard(taxeAnnuelle.getNbrMoisRetard());
+                notifications.add(notification);
+            }
+            return notifications;
+        }
+    }
 }
